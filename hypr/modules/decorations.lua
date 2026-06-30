9ade6879035ab1d657377f89fd0340f343a4d985
Triangulation5 <joshua.jomet@gmail.com>
2026-06-30 13:02:31 -0400

    feat: change hypr, nvim, and waybar config

--- hypr/modules/decorations.lua
+++ hypr/modules/decorations.lua
@@ -23,7 +23,7 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 10,
+        rounding       = 05,
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
