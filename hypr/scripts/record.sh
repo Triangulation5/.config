d6706239cfbec2d157a3fbf4597c7f9c2617cd52
Triangulation5 <joshua.jomet@gmail.com>
2026-08-06 21:26:50 -0400

    temp: trying out old shell

--- hypr/scripts/record.sh
+++ hypr/scripts/record.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mon=$(hyprctl activeworkspace -j | jq -r '.monitor')
-qs -c silhouette-shell ipc call pill quickRecord "$mon"
+# qs -c silhouette-shell ipc call pill quickRecord "$mon"
 
 # Old monolithic pill
-# qs -c pill ipc call pill quickRecord "$mon"
+qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill quickRecord "$mon"
