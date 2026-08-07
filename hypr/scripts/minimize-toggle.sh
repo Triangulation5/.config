02bb26f56d97e0800c1dac6550d904880c1d18f5
Triangulation5 <joshua.jomet@gmail.com>
2026-08-06 21:29:28 -0400

    feat: went back to new shell

--- hypr/scripts/minimize-toggle.sh
+++ hypr/scripts/minimize-toggle.sh
@@ -8,11 +8,11 @@ ws=$(printf '%s' "$aw" | jq -r '.workspace.name // ""')
 
 if [ "$ws" = "special:minimized" ]; then
 	real=$(hyprctl monitors -j | jq -r 'map(select(.focused)) | .[0].activeWorkspace.id // 1')
-	# qs -c silhouette-shell ipc call pill restoreWindow "$addr|$real"
+	qs -c silhouette-shell ipc call pill restoreWindow "$addr|$real"
     # Old monolithic pill
-    qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill restoreWindow "$addr|$real"
+    # qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill restoreWindow "$addr|$real"
 else
-	# qs -c silhouette-shell ipc call pill minimizeWindow "$addr"
+	qs -c silhouette-shell ipc call pill minimizeWindow "$addr"
     # Old monolithic pill
-	qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill minimizeWindow "$addr"
+	# qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill minimizeWindow "$addr"
 fi
