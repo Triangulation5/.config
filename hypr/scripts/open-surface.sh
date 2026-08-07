ab23545c36028fe168ec493fe6d2e5063ea4b7b1
Triangulation5 <joshua.jomet@gmail.com>
2026-08-06 21:29:28 -0400

    feat: went back to new shell

--- hypr/scripts/open-surface.sh
+++ hypr/scripts/open-surface.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mon=$(hyprctl activeworkspace -j | jq -r '.monitor')
-# qs -c silhouette-shell ipc call pill "$1" "$mon"
+qs -c silhouette-shell ipc call pill "$1" "$mon"
 
 # Old monolithic pill
-qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill "$1" "$mon"
+# qs -p ~/.config/quickshell/monolithic-shell/pill ipc call pill "$1" "$mon"
--- hypr/scripts/reload.sh
+++ hypr/scripts/reload.sh
@@ -2,9 +2,9 @@
 
 pkill qs
 
-# qs -p ~/.config/quickshell/silhouette-shell/ & disown
+qs -p ~/.config/quickshell/silhouette-shell/ & disown
 
 # Old monolithic pill
-qs -p ~/.config/quickshell/monolithic-shell/pill/shell.qml & disown
-qs -p ~/.config/quickshell/monolithic-shell/lock/ & disown
-qs -p ~/.config/quickshell/monolithic-shell/screencorner/ & disown
+# qs -p ~/.config/quickshell/monolithic-shell/pill/shell.qml & disown
+# qs -p ~/.config/quickshell/monolithic-shell/lock/ & disown
+# qs -p ~/.config/quickshell/monolithic-shell/screencorner/ & disown
